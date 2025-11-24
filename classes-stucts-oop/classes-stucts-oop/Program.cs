33d9d6fe76adf8c42572db365fa6b59b3245fa70
mksigrest <mksigrest@gmail.com>
2025-11-23 21:32:20 -0500

    up to step 3

--- classes-stucts-oop/classes-stucts-oop/Program.cs
+++ classes-stucts-oop/classes-stucts-oop/Program.cs
@@ -4,13 +4,15 @@
     {
         public class Character
         {
-
+            public string name;
+            public int exp;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
             Character hero = new Character();
+            Console.WriteLine("Hero: " + hero.name + " - " + hero.exp + " EXP");
         }
     }
 }
